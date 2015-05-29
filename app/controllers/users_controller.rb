9f38587a291fc6859936d670a16610a1fb4c13e0
Annie <annotating@gmail.com>
2015-05-28 23:24:56 -0700

    tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_filter :verify_login, :except => [:about]
+	
 	def show
 	    @user = User.find(params[:id])
 	end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,5 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options = { host: 'https://pacific-harbor-6760.herokuapp.com' }  
+  config.active_record.dump_schema_after_migration = false 
 end
